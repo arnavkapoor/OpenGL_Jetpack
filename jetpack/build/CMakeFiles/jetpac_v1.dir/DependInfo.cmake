f0d631ca639321e9bef16c2bcf0fc4f9b258598c
Arnav <arnavk805@gitlab.com>
2019-01-16 18:06:01 +0530

    build folder cmak

---
+++ jetpack/build/CMakeFiles/jetpac_v1.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/arnav/Courses2-2/Graphics/OpenGL_Jetpack/jetpack/src/ball.cpp" "/home/arnav/Courses2-2/Graphics/OpenGL_Jetpack/jetpack/build/CMakeFiles/jetpac_v1.dir/src/ball.cpp.o"
+  "/home/arnav/Courses2-2/Graphics/OpenGL_Jetpack/jetpack/src/color.cpp" "/home/arnav/Courses2-2/Graphics/OpenGL_Jetpack/jetpack/build/CMakeFiles/jetpac_v1.dir/src/color.cpp.o"
+  "/home/arnav/Courses2-2/Graphics/OpenGL_Jetpack/jetpack/src/input.cpp" "/home/arnav/Courses2-2/Graphics/OpenGL_Jetpack/jetpack/build/CMakeFiles/jetpac_v1.dir/src/input.cpp.o"
+  "/home/arnav/Courses2-2/Graphics/OpenGL_Jetpack/jetpack/src/main.cpp" "/home/arnav/Courses2-2/Graphics/OpenGL_Jetpack/jetpack/build/CMakeFiles/jetpac_v1.dir/src/main.cpp.o"
+  "/home/arnav/Courses2-2/Graphics/OpenGL_Jetpack/jetpack/src/nonedit.cpp" "/home/arnav/Courses2-2/Graphics/OpenGL_Jetpack/jetpack/build/CMakeFiles/jetpac_v1.dir/src/nonedit.cpp.o"
+  "/home/arnav/Courses2-2/Graphics/OpenGL_Jetpack/jetpack/src/other_handlers.cpp" "/home/arnav/Courses2-2/Graphics/OpenGL_Jetpack/jetpack/build/CMakeFiles/jetpac_v1.dir/src/other_handlers.cpp.o"
+  "/home/arnav/Courses2-2/Graphics/OpenGL_Jetpack/jetpack/src/timer.cpp" "/home/arnav/Courses2-2/Graphics/OpenGL_Jetpack/jetpack/build/CMakeFiles/jetpac_v1.dir/src/timer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ jetpack/build/CMakeFiles/jetpac_v1.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jetpac_v1.dir/src/other_handlers.cpp.o"
+  "CMakeFiles/jetpac_v1.dir/src/ball.cpp.o"
+  "CMakeFiles/jetpac_v1.dir/src/timer.cpp.o"
+  "CMakeFiles/jetpac_v1.dir/src/color.cpp.o"
+  "CMakeFiles/jetpac_v1.dir/src/input.cpp.o"
+  "CMakeFiles/jetpac_v1.dir/src/nonedit.cpp.o"
+  "CMakeFiles/jetpac_v1.dir/src/main.cpp.o"
+  "jetpac_v1.pdb"
+  "jetpac_v1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jetpac_v1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
